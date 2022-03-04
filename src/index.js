fe76c1ef6fd5ec8c5c53d967226cc90024dfb2a5
biruk166 <hailubruk1@gmail.com>
2022-03-03 22:35:01 -0600

    test commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+function index() {
+  return ( 
+    <App/>
+   );
+}
+
+
+
+ReactDOM.render(<index/>, document.getElementById('root'));
+
