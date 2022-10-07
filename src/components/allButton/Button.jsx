9fdd80b1b7a913d787601b5056ed49089111e994
biruk166 <hailubruk1@gmail.com>
2022-10-06 23:03:36 -0500

    project changed

--- src/components/allButton/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './button.css';
-
-export const Button = (props) => {
-  return (
-    <button className='allbutton'>{props.name}</button>
-  )
-}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-function index() {
-  return ( 
-    <App/>
-   );
-}
-
-
-
-ReactDOM.render(<index/>, document.getElementById('root'));
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
